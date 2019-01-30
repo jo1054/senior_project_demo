1a3120b5cf217e9023d7c1c3b524160fceac2322
Jonathan <jonathan.ojeda01@utrgv.edu>
2019-01-30 16:56:18 -0600

    calorie info

--- firebase_integration/lib/pages/FirstPage.dart
+++ firebase_integration/lib/pages/FirstPage.dart
@@ -17,12 +17,26 @@ class FirstState extends State<First> with SingleTickerProviderStateMixin {
 
   final TextEditingController _hightController=new TextEditingController();
   final TextEditingController _weightController=new TextEditingController();
+  final TextEditingController _ageController=new TextEditingController();
+
   int _weightRadioValue=0;
+  int _activityRadioValue=0;
+  int _ageRadioValue=0;
   int _hightRadioValue=0;
+  int _genderRadioValue=0;
+
   double weightmultiplier=1.0;
   double hightmultiplier=0.01;
+  double activitymultiplier=0.01;
+  double gendermultiplier=0.01;
   int bmiResult=0;
+  int protien =0;
+  int carbs = 0;
+  int fat =0;
   String info="";
+  String info2="";
+  String info3="";
+  String info4="";
   Color stateColor=Colors.white;
 
   AnimationController animController;
@@ -62,7 +76,7 @@ class FirstState extends State<First> with SingleTickerProviderStateMixin {
               fit: BoxFit.cover)),
     );
 
-    final greenOpacity = Container(color: Color(0xAA69F0CF),);
+    final greenOpacity = Container(color: Color(0xf2f3f3),);
 
     final logo =FadeTransition(
       opacity: animation,
@@ -78,18 +92,30 @@ class FirstState extends State<First> with SingleTickerProviderStateMixin {
           child: new Container(
               child: new Column(
                   children: <Widget>[
-
+                    new TextField(
+                      keyboardType: TextInputType.number,
+                      style: textStyle.copyWith(fontSize: 16.0,fontStyle: FontStyle.normal),
+                      controller: _ageController ,
+                      cursorColor: Colors.black,
+                      decoration: new InputDecoration(
+                        labelText: "Age",
+                        labelStyle: textStyle.copyWith(fontSize: 19.0,fontStyle: FontStyle.normal),
+                        hintText: "Enter your age",
+                        hintStyle: new TextStyle(color: Colors.black),
+                        icon: new Icon(Icons.line_weight,color: Colors.black,),
+                      ),
+                    ),
                     new TextField(
                       keyboardType: TextInputType.number,
                       style: textStyle.copyWith(fontSize: 16.0,fontStyle: FontStyle.normal),
                       controller: _weightController ,
-                      cursorColor: Colors.white,
+                      cursorColor: Colors.black,
                       decoration: new InputDecoration(
                         labelText: "Weight",
                         labelStyle: textStyle.copyWith(fontSize: 19.0,fontStyle: FontStyle.normal),
                         hintText: "Enter your weight",
-                        hintStyle: new TextStyle(color: Colors.white70),
-                        icon: new Icon(Icons.accessibility_new,color: Colors.white),
+                        hintStyle: new TextStyle(color: Colors.black),
+                        icon: new Icon(Icons.accessibility_new,color: Colors.black),
                       ),
                     ),
                     new Row(
@@ -115,13 +141,14 @@ class FirstState extends State<First> with SingleTickerProviderStateMixin {
                       keyboardType: TextInputType.number,
                       style: textStyle.copyWith(fontSize: 16.0,fontStyle: FontStyle.normal),
                       controller: _hightController ,
-                      cursorColor: Colors.white,
+                      cursorColor: Colors.black,
+
                       decoration: new InputDecoration(
                         labelText: "Hight",
                         labelStyle: textStyle.copyWith(fontSize: 19.0,fontStyle: FontStyle.normal),
                         hintText: "Enter your hight",
-                        hintStyle: new TextStyle(color: Colors.white70),
-                        icon: new Icon(Icons.line_weight,color: Colors.white,),
+                        hintStyle: new TextStyle(color: Colors.black),
+                        icon: new Icon(Icons.line_weight,color: Colors.black,),
                       ),
                     ),
                     new Row(
@@ -142,18 +169,62 @@ class FirstState extends State<First> with SingleTickerProviderStateMixin {
 
                         ]
                     ),
-                    new TextField(
-                      keyboardType: TextInputType.number,
-                      style: textStyle.copyWith(fontSize: 16.0,fontStyle: FontStyle.normal),
-                      controller: _hightController ,
-                      cursorColor: Colors.white,
-                      decoration: new InputDecoration(
-                        labelText: "Male or Female",
-                        labelStyle: textStyle.copyWith(fontSize: 19.0,fontStyle: FontStyle.normal),
-                        hintText: "",
-                        hintStyle: new TextStyle(color: Colors.white70),
-                        icon: new Icon(Icons.line_weight,color: Colors.white,),
-                      ),
+
+                    new Row(
+                        mainAxisAlignment:  MainAxisAlignment.center,
+                        children: <Widget>[
+                          new Radio<int>(
+                              value: 0,
+                              groupValue: _genderRadioValue,
+                              onChanged: handleGenderRadioValueChange
+                          ),
+                          new Text("Male",style: new TextStyle(fontSize: 17.0),),
+                          new Radio<int>(
+                              value: 1,
+                              groupValue: _genderRadioValue,
+                              onChanged: handleGenderRadioValueChange
+                          ),
+                          new Text("Female",style: new TextStyle(fontSize: 17.0),),
+
+                        ]
+                    ),
+                    new Row(
+                        mainAxisAlignment:  MainAxisAlignment.center,
+                        children: <Widget>[
+
+                          new Radio<int>(
+                              value: 1,
+                              groupValue: _activityRadioValue,
+                              onChanged: handleActivityRadioValueChange
+                          ),
+                          new Text("Sedentary",style: new TextStyle(fontSize: 17.0),),
+                          new Radio<int>(
+                              value: 2,
+                              groupValue: _activityRadioValue,
+                              onChanged: handleActivityRadioValueChange
+                          ),
+                          new Text("Light activity",style: new TextStyle(fontSize: 17.0),),
+
+                        ]
+                    ),
+                    new Row(
+                        mainAxisAlignment:  MainAxisAlignment.center,
+                        children: <Widget>[
+
+                          new Radio<int>(
+                              value: 3,
+                              groupValue: _activityRadioValue,
+                              onChanged: handleActivityRadioValueChange
+                          ),
+                          new Text("Moderate activity",style: new TextStyle(fontSize: 17.0),),
+                          new Radio<int>(
+                              value: 4,
+                              groupValue: _activityRadioValue,
+                              onChanged: handleActivityRadioValueChange
+                          ),
+                          new Text("Very active",style: new TextStyle(fontSize: 17.0),),
+
+                        ]
                     ),
                   ]
               )
@@ -161,7 +232,7 @@ class FirstState extends State<First> with SingleTickerProviderStateMixin {
         )
     );
 
-    final result = new Text(bmiResult==0? '$info' : '$info'+' $bmiResult',textAlign: TextAlign.center,
+    final result = new Text(bmiResult==0? '$info' : '$info'+' $bmiResult' +'$info2 ' + ' $protien'+'$info3'+'$fat'+'$info4'+'$carbs',textAlign: TextAlign.center,
       style: textStyle.copyWith(fontSize: 20.0,fontStyle: FontStyle.italic),);
 
     final analysis = new Text(bmiResult >=10 ? state(bmiResult) : "" ,textAlign: TextAlign.center,
@@ -270,30 +341,92 @@ class FirstState extends State<First> with SingleTickerProviderStateMixin {
       switch (value) {
 
         case 0:
-          hightmultiplier=0.01;
+          hightmultiplier=1.0;
           break;
 
         case 1:
-          hightmultiplier=1.0;
+          hightmultiplier=100;
           break;
       }
     });
   }
 
+  handleGenderRadioValueChange(int value){
+
+    setState(() {
+      _genderRadioValue=value;
+
+      switch (value) {
+
+        case 0:
+          gendermultiplier=5;
+          break;
+
+        case 1:
+          gendermultiplier=-161;
+          break;
+      }
+    });
+  }
+
+  handleActivityRadioValueChange(int value){
+
+    setState(() {
+      _activityRadioValue=value;
+
+      switch (value) {
+
+        case 0:
+          activitymultiplier=1.2;
+          break;
+
+        case 1:
+          activitymultiplier=1.375;
+          break;
+        case 2:
+          activitymultiplier=1.55;
+          break;
+
+        case 4:
+          activitymultiplier=1.725;
+          break;
+      }
+    });
+  }
   calculateBMI(){
     double weight=0.0;
     double hight=0.0;
+    double age = 0;
 
+    double pm = 4;
+    double cm = 4;
+    double fm = 9;
+
+    double pc = 0;
+    double cc = 0;
+    double fc = 0;
     setState(() {
 
       if(_weightController.text.isNotEmpty && double.parse(_weightController.text) > 0.0 && _hightController.text.isNotEmpty &&  double.parse(_hightController.text) > 0.0){
+        age= double.parse(_ageController.text);
         weight=double.parse(_weightController.text);
         hight=double.parse(_hightController.text);
         double weightF=weight*weightmultiplier;
         double hightF=hight*hightmultiplier;
-        if((weightF/(hightF*hightF))>=10 && (weightF/(hightF*hightF))<=100){
-          info="your current BMI ratio is";
-          bmiResult=(weightF/(hightF*hightF)).round();
+        if(age !=0 || weight != 0 || hight != 0 ){
+          info="your calorie estimate is " ;
+          bmiResult=(((10*weightF) + (6.25*hightF) -(5*age)+(gendermultiplier))*activitymultiplier).round();
+          protien = (weightF * 1.815).round();
+          fat = ((bmiResult *.25)/9).round();
+          pc = protien*pm;
+          fc = fat*fm;
+          cc = bmiResult-pc - fc.round();
+          carbs = (cc/9).round();
+          info2="\nprotien:";
+          info3=" fat: ";
+          info4=" carbs: ";
+
+          //bmiResult=(weightF/(hightF*hightF)).round();
         }else{
           bmiResult=0;
           info="please enter correct values";
@@ -309,16 +442,16 @@ class FirstState extends State<First> with SingleTickerProviderStateMixin {
 
   String state(int bmi){
 
-    if( bmi < 18.5 ) { stateColor=Colors.red; return "( Underweight )"; }
-    else if( bmi >= 18.5 && bmi <= 24.99 ) { stateColor=Colors.green.shade400; return "( Healty Weight )"; }
-    else if( bmi >= 25 && bmi <= 29.99 ) { stateColor=Colors.yellow; return "( Overweight )"; }
-    else if( bmi >= 30 ) { stateColor=Colors.red; return "( Obese )"; }
+    if( bmi < 18.5 ) { stateColor=Colors.red; return ""; }
+    else if( bmi >= 18.5 && bmi <= 24.99 ) { stateColor=Colors.green.shade400; return ""; }
+    else if( bmi >= 25 && bmi <= 29.99 ) { stateColor=Colors.yellow; return ""; }
+    else if( bmi >= 30 ) { stateColor=Colors.red; return ""; }
 
   }
 
 }
 
 const TextStyle textStyle=TextStyle(
-    color: Colors.white,
+    color: Colors.black,
     fontStyle: FontStyle.italic
     ,fontFamily: 'BebasNeue');
